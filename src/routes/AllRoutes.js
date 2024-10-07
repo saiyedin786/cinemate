f5199123b4baa65fe8530c586f5207cb521fbbf3
saiyedin786 <saiyedin786@gmail.com>
2024-10-07 15:20:16 +0530

    update5

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -7,11 +7,11 @@ export const AllRoutes = () => {
     <Routes>
         <Route path='/' element={<MovieList apipath="movie/now_playing"  title="Home"/>} />
         <Route path='/movie/:id' element={<MovieDetail/>} />
-        <Route path='/movies/popular' element={<MovieList apipath="/movie/popular" title="popular/cinemate"/>} />
-        <Route path='/movies/top' element={<MovieList apipath="/movie/top_rated"  title="TOp/cinemate"/>} />
-        <Route path='/movies/upcoming' element={<MovieList apipath="/movie/upcoming"  title="upcoming/cinemate"/>} />
-        <Route path='search' element={<Search apipath="/search/movie"  title="search/cinemate"/>} />
-        <Route path='*' element={<PageNotFound  title="/pagenotfound/cinemate"/> } />
+        <Route path='/movies/popular' element={<MovieList apipath="movie/popular" title="popular/cinemate"/>} />
+        <Route path='/movies/top' element={<MovieList apipath="movie/top_rated"  title="TOp/cinemate"/>} />
+        <Route path='/movies/upcoming' element={<MovieList apipath="movie/upcoming"  title="upcoming/cinemate"/>} />
+        <Route path='search' element={<Search apipath="search/movie"  title="search/cinemate"/>} />
+        <Route path='*' element={<PageNotFound  title="pagenotfound/cinemate"/> } />
     </Routes>
   )
 }
